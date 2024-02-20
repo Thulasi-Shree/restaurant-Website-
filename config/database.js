a528cdca1eef05591af149439d1c9b7cdc45c7f7
Thulasi-Shree <thulasi9941@gmail.com>
2024-02-21 01:04:47 +0530

    updated

--- config/database.js
+++ config/database.js
@@ -1,13 +1,30 @@
+// const mongoose = require('mongoose');
+
+
+// const connectDatabase = ()=>{
+//     mongoose.connect('mongodb://127.0.0.1:27017/schemaDraft',{
+//         useNewUrlParser:true,
+//         useUnifiedTopology:true
+//     }).then(con=>{
+//         console.log(`MongoDB is connected to the host: ${con.connection.host} `)
+//     })
+// }
+
+// module.exports = connectDatabase;  
 const mongoose = require('mongoose');
+require('dotenv').config();
 
+const connectDatabase = () => {
+    const atlasConnectionUri = process.env.MONGODB_URI;
 
-const connectDatabase = ()=>{
-    mongoose.connect('mongodb://127.0.0.1:27017/schemaDraft',{
-        useNewUrlParser:true,
-        useUnifiedTopology:true
-    }).then(con=>{
-        console.log(`MongoDB is connected to the host: ${con.connection.host} `)
-    })
+    mongoose.connect(atlasConnectionUri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(con => {
+        console.log(`MongoDB is connected to the host: ${con.connection.host}`);
+    }).catch(err => {
+        console.error(`Error connecting to MongoDB: ${err}`);
+    });
 }
 
-module.exports = connectDatabase;  
+module.exports = connectDatabase;
